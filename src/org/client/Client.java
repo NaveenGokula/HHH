e53ce2c70f4c56a8ea2dcfd813e3e8eae60579aa
NaveenGokula <nk05437@gmail.com>
2023-06-07 18:07:01 +0530

    hjgfsd

--- src/org/client/Client.java
+++ src/org/client/Client.java
@@ -2,7 +2,9 @@ package org.client;
 
 public class Client {
 	public void ClientName() {
-System.out.println("client name= kanmani");
+System.out.println("client ");
 	}
+	public Client() {
+System.out.println("client name=priya");	}
 
 }
